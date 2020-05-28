1c0cf9036a706034a5a90a2e1689073e08c8f7c0
MICHAEL-MAURICE <52583744+MICHAEL-MAURICE@users.noreply.github.com>
2020-05-28 03:17:37 +0200

    this ui for gaming

---
+++ android/app/src/main/kotlin/go/mic/game_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package go.mic.game_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
